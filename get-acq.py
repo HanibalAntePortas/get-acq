3ab0b77ee0f87cad5a3bd7be73584dd8b7ae897e
Milan <89018933+kljunowsky@users.noreply.github.com>
2021-11-10 08:19:36 +0100

    Add files via upload

---
+++ get-acq.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import requests
+import json
+import argparse
+
+
+#parser = argparse.ArgumentParser(add_help=True)
+#parser.add_argument('-o', '--organisation', help='Specify the file containing organisation names (lovercase)')
+
+parser = argparse.ArgumentParser(add_help=True)
+parser.add_argument('-d', '--domains', help='Specify the list with  domains')
+parser.add_argument('-a', '--apikey', help='Provide securitytrails API key')
+parser.add_argument('-o', '--output', help='Specify output filename')
+
+
+args = parser.parse_args()
+apikey = args.apikey
+output = args.output
+
+
+with open(args.domains, 'r') as f:
+	for domain in f:
+		domain = domain.strip()
+		url = "https://api.securitytrails.com/v1/domain/"+domain+"/acquisitions"
+		headers = {"apikey": apikey}
+		response = requests.get(url, headers=headers)
+		#response = requests.get(url)
+
+		#print(url)
+		json_data = json.loads(response.text)
+		#print(json_data)
+		#print(apikey)
+
+with open(output, "a") as o:
+	o.write(str(json_data))
